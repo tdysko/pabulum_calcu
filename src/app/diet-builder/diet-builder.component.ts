54101722a663979e4602529807ca18871c4fdce2
Tomasz Dysko <test@test.com>
2019-09-18 22:55:18 +0200

    elements calculators

--- src/app/diet-builder/diet-builder.component.ts
+++ src/app/diet-builder/diet-builder.component.ts
@@ -12,8 +12,17 @@ export class DietBuilderComponent implements OnInit {
   keyword = 'name';
   data = this.productsService.products;
   dietProducts: Array<Product> = new Array<Product>();
+  dietSummary: Product;
 
-  constructor(@Inject(ProductsService) private productsService) { }
+  constructor(@Inject(ProductsService) private productsService) {
+    this.dietSummary = {
+      id: 0,
+      proteins: 0,
+      name: "Summary",
+      carbs: 0,
+      fats: 0
+    };
+  }
 
 
   ngOnInit() {
@@ -21,7 +30,44 @@ export class DietBuilderComponent implements OnInit {
 
   selectEvent(item) {
     this.dietProducts.push(item);
+    this.addToSummary(item);
     console.log(item);
+  }
+
+  getSummaryProteins(): number {
+    var proteins: number = 0;
+    for (const product of this.dietProducts) {
+      proteins = proteins + product.proteins;
+    }
+    return proteins;
+  }
+
+  getSummaryCarbs(): number {
+    var carbs: number = 0;
+    for (const product of this.dietProducts) {
+      carbs = carbs + product.proteins;
+    }
+    return carbs;
+  }
+
+  getSummaryFats(): number {
+    var fats: number = 0;
+    for (const product of this.dietProducts) {
+      fats = fats + product.proteins;
+    }
+    return fats;
+  }
+
+  addToSummary(product) {
+    this.dietSummary.proteins = parseFloat((this.dietSummary.proteins + product.proteins).toPrecision(5));
+    this.dietSummary.carbs = parseFloat((this.dietSummary.carbs + product.carbs).toPrecision(5));
+    this.dietSummary.fats = parseFloat((this.dietSummary.fats + product.fats).toPrecision(5));
+  }
+
+  removeFromSummary(product) {
+    this.dietSummary.proteins = parseFloat((this.dietSummary.proteins - product.proteins).toPrecision(5));
+    this.dietSummary.carbs = parseFloat((this.dietSummary.carbs - product.carbs).toPrecision(5));
+    this.dietSummary.fats = parseFloat((this.dietSummary.fats - product.fats).toPrecision(5));
   }
 
   onChangeSearch(val: string) {
@@ -34,6 +80,7 @@ export class DietBuilderComponent implements OnInit {
   }
 
   deleteFieldValue(i) {
+    this.removeFromSummary(this.dietProducts[i]);
     this.dietProducts.splice(i, 1);
   }
 }

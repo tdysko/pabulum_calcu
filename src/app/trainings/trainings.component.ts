c3ce483c68e185218b02ffc9c7afd57815525068
Tomasz Dysko <tdysko@future-processing.com>
2020-11-11 18:41:36 +0100

    Upgrade, build fix and gpx parsing

--- src/app/trainings/trainings.component.ts
+++ src/app/trainings/trainings.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import GPX from 'gpx-parser-builder';
 
 @Component({
   selector: 'app-trainings',
   templateUrl: './trainings.component.html',
   styleUrls: ['./trainings.component.sass']
 })
-export class TrainingsComponent implements OnInit {
+export class TrainingsComponent {
+  fileToUpload: File = null;
+  gpxtext: string | ArrayBuffer;
 
-  constructor() {
+  handleFileInput(files: FileList) {
+    let fileReader = new FileReader();
+    this.fileToUpload = files.item(0);
+    let text: string | ArrayBuffer;
+    let that = this;
+
+    fileReader.onload = function (e) {
+      console.log(fileReader.result);
+      that.gpxtext = fileReader.result;
+      that.parseFile(that.gpxtext);
+    }
+    fileReader.readAsText(this.fileToUpload);
   }
 
-  ngOnInit() {
-  }
+  parseFile(fileString: any) {
+    console.log('parsing: ' + this.gpxtext);
 
+    const gpx = GPX.parse(this.gpxtext);
+
+    console.log(gpx);
+  }
 }
